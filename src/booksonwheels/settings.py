ef639e90b73b471d6427ab217d56cfa5ad57f5a3
print123 <shaunak.thaker@gmail.com>
2016-02-22 08:11:24 +0530

    With models.py

--- src/booksonwheels/settings.py
+++ src/booksonwheels/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'booksonwheels.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'booksonwheels',
+        'USER': 'postgres',
+        'PASSWORD' : 'Programmer@08',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
